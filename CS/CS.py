754eea56533b163062d74e544fc043c798067877
araag2 <artur.guimas@gmail.com>
2018-10-03 06:59:12 -0400

    CS basic strucuture and this

--- CS/CS.py
+++ CS/CS.py
@@ -1,16 +1,35 @@
-#! /usr/bin/python3
-import sys
+import sys 
 import socket
+import json
+import os
+from CSFunctions import *
+
+userUsername = [99999]
+userPassword = ['zzzzzzzz']
+buffersize = 256
+
+newPID = os.fork()
+if newPID == 0:
+	UDPConnections()
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serverSocket.bind(('localhost', 80))
+while 1:
+	newPID = 1;
+	serverSocket.bind(('localhost', 84))
+	serverSocket.listen(5)	
+	(clientSocket, address) = serverSocket.accept()
+	os.fork()
+	if newPID == 0:
+		break
 
-serverSocket.listen(5)
+msgRecv = mySocket.recv(buffersize)
+msgRecv = msgRecv.decode()
+print(msgRecv)
 
-(clientSocket, address) = serverSocket.accept()
+if msgRecv[0:len('AUT ')] == 'AUT ':
 
-while True:
-	message = clientSocket.recv(230)
-	print(message.decode())	
-	clientSocket.send(input().encode())
+	if msgRecv[4:9] == 99999 and msgRecv[11:19] == 'zzzzzzzz':
+		mySocket.send('OK')
+	else:
+		mySocket.send('NOK')
---
+++ CS/CSFunctions.py
@@ -0,0 +1,6 @@
+#Piece of code responsible for constantly dealing with the
+#BS UDP connections
+def UDPConnections():
+	print('Eu existo e cenas, obrigado por tudo pai')
+	while 1:
+		i=0
