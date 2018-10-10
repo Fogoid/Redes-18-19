346f30a61fb0a329538c0faa25658dd50d9ee010
Fogoid <diogo.barradas.fernandes@gmail.com>
2018-10-10 11:45:45 +0100

    made LFU and LSU functions

--- CS/CSBSFunctions.py
+++ CS/CSBSFunctions.py
@@ -1,3 +1,23 @@
 import socket
 import sys
 import os
+from CSBaseFunctions import *
+
+def LSFCommand(BSSocket, BSIP, BSport, username, dirname):
+	lsfMsg = 'LSF ' + username + ' ' + dirname + '\n'
+	msgRecv = communicateUDP(lsfMsg, BSIP, BSport, BSSocket)
+	splitedMsg = msgRecv.split(' ')
+	if CMDMatcher(msgRecv, '^LFD\s[0-9]\s'):
+		if len(splitedMsg[2:]) == 4*splitedMsg[1]:
+			return splitedMsg[1:]
+	elif CMDMatcher(msgRecv, '^ERR$'):
+		return 'ERR'
+
+def LSUCommand(BSSocket, BSIP, BSport, username, password):
+	lsuMsg = 'DLB ' + username + ' ' + password + '\n'
+	msgRecv = communicateUDP(lsuMsg, BSIP, BSport, BSSocket)
+	splitedMsg = msgRecv.split(' ')
+	if CMDMatcher(msgRecv, '^LUR\s[A-Z]\n$'):
+		return splitedMsg[1]
+	elif CMDMatcher(msgRecv, '^ERR\n$'):
+		return 'ERR'
